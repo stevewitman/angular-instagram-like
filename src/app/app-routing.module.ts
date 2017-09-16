38c0885224bcacbb8977b52f1be1703cd0e1d583
Steve Witman <stevewitman@gmail.com>
2017-09-15 23:38:29 -0600

    Scaffold components and wire up routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { FollowingComponent } from './following/following.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MyPostsComponent } from './my-posts/my-posts.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { LoginComponent } from './auth/login/login.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'allposts', component: AllPostsComponent },
+    { path: 'following', component: FollowingComponent },
+    { path: 'favorites', component: FavoritesComponent },
+    { path: 'myposts', component: MyPostsComponent },
+    { path: 'signup', component: SignUpComponent },
+    { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { FollowingComponent } from './following/following.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MyPostsComponent } from './my-posts/my-posts.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AllPostsComponent,
+    FollowingComponent,
+    FavoritesComponent,
+    MyPostsComponent,
+    SignUpComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
