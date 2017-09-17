24ca8318e91e7aa5c21647601f2b2080fb32fde1
Steve Witman <stevewitman@gmail.com>
2017-09-16 22:43:50 -0600

    Install firebase, wireup signup with email verification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    const config = {
+      apiKey: "AIzaSyDNn0Mz3Xw2FO3j-Al3j9kV9dDwE2u0jOE",
+      authDomain: "demogram-d9e9b.firebaseapp.com",
+      databaseURL: "https://demogram-d9e9b.firebaseio.com",
+      projectId: "demogram-d9e9b",
+      storageBucket: "demogram-d9e9b.appspot.com",
+      messagingSenderId: "1063434137426"
+    };
+    firebase.initializeApp(config);
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    firebase.auth().onAuthStateChanged(userData => {
+      if (userData && userData.emailVerified) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    })
   }
 
 }
